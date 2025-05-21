4d3d84f100aee968a2458379175182b9e3dd9e9b
R1p33r17 <marioprovahk@gmail.com>
2025-05-21 21:00:13 +0200

    rinominato in prova.py e modificato per chiedere il nome

---
+++ prova.py
@@ -0,0 +1,6 @@
+name = input('Come ti chiami?:\n ')
+print(f'ciao {name}!! siamo lieti di comunicarti la tua età')
+birth_year = int(input(f'when does {name} born? Input your year..\n '))
+actual_year= 2025
+age= actual_year - birth_year
+print(f'You\'re ... \n {age} !!!')
