a15ca6b2a063366f11179fc3cfebdd90641c3382
R1p33r17 <marioprovahk@gmail.com>
2025-05-22 18:45:32 +0200

    new translate

--- prova.py
+++ prova.py
@@ -1,5 +1,5 @@
 name = input('What\'s your name?:\n ')
-print(f'ciao {name}!! siamo lieti di comunicarti la tua età')
+print(f'Hi {name}!! We are happy to say you your age!')
 birth_year = int(input(f'when does {name} born? Input your year..\n '))
 actual_year= 2025
 age= actual_year - birth_year
